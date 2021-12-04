27a9a8f5713a7c969d9d02942b62cd06692f05ed
Sasha <aleksagerhardt@gmail.com>
2021-12-04 22:57:34 +0300

    changing trapezoid function

--- Trapezoid.cpp
+++ Trapezoid.cpp
@@ -3,3 +3,36 @@
 #include <math.h>
 #include "Trapezoid.h"
 
+Trapezoid Trapezoid::settrap(Trapezoid trap)
+{
+    printf("\nДлина основания а: ");
+    scanf("%lf", &trap.dlinaa);
+    printf("\nДлина основания b: ");
+    scanf("%lf", &trap.dlinab);
+    printf("\nДлина стороны c: ");
+    scanf("%lf", &trap.dlinac);
+    printf("\nДлина стороны d: ");
+    scanf("%lf", &trap.dlinad);
+    printf("\nДлина высоты h: ");
+    scanf("%lf", &trap.dlinah);
+    return trap;
+}
+
+void Trapezoid::printtrap(Trapezoid trap)
+{
+    printf("\nДлина оснований a и b, сторон c и d и высоты h соответственно: %lf, %lf, %lf, %lf, %lf", trap.dlinaa, trap.dlinab, trap.dlinac, trap.dlinad, trap.dlinah);
+}
+
+double Trapezoid::trapperimeter(Trapezoid trap)
+{
+    double perimetr = 0;
+    perimetr = trap.dlinaa + trap.dlinab + trap.dlinac + trap.dlinad;
+    return perimetr;
+}
+
+double Trapezoid::trapsurf(Trapezoid trap)
+{
+    double volume = 0;
+    volume = ((trap.dlinaa * trap.dlinab) / 2) * trap.dlinah;
+    return volume;
+}
--- Trapezoid.h
+++ Trapezoid.h
@@ -6,7 +6,10 @@ class Trapezoid {
 private:
 	double dlinaa, dlinab, dlinac, dlinad, dlinah;
 public:
-	
+	Trapezoid settrap(Trapezoid trap);
+	void printtrap(Trapezoid trap);
+	double trapperimeter(Trapezoid trap);
+	double trapsurf(Trapezoid trap);
 };
 
 #endif
