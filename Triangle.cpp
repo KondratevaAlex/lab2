f653d5a526279821f157f332737505cbf1bbf642
Sasha <aleksagerhardt@gmail.com>
2021-12-04 23:01:53 +0300

    changing triangle function

--- Triangle.cpp
+++ Triangle.cpp
@@ -3,3 +3,33 @@
 #include <math.h>
 #include "Triangle.h"
 
+Triangle Triangle::settri(Triangle trian)
+{
+	printf("\nДлина стороны а: ");
+	scanf("%lf", &trian.dlinaa);
+	printf("Длина стороны b: ");
+	scanf("%lf", &trian.dlinab);
+	printf("Длина стороны c: ");
+	scanf("%lf", &trian.dlinac);
+	return trian;
+}
+
+void Triangle::printtri(Triangle trian)
+{
+	printf("\nДлины сторон а, b и c соответственно: %lf, %lf, %lf", trian.dlinaa, trian.dlinab, trian.dlinac);
+}
+
+double Triangle::triperimeter(Triangle trian)
+{
+	double perimetr = 0;
+	perimetr = trian.dlinaa + trian.dlinab + trian.dlinac;
+	return perimetr;
+}
+
+double Triangle::trisurf(Triangle trian)
+{
+	double volume = 0, perim = 0;
+	perim = (trian.dlinaa + trian.dlinab + trian.dlinac) / 2;
+	volume = sqrt(perim * (perim - trian.dlinaa) * (perim - trian.dlinab) * (perim - trian.dlinac));
+	return volume;
+}
--- Triangle.h
+++ Triangle.h
@@ -6,7 +6,10 @@ class Triangle {
 private:
 	double dlinaa, dlinab, dlinac;
 public:
-	
+	Triangle settri(Triangle trian);
+	void printtri(Triangle trian);
+	double triperimeter(Triangle trian);
+	double trisurf(Triangle trian);
 };
 
 #endif
